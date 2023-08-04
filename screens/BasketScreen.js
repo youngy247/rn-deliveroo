cb400a54da3c45b895a361385f956e33176469eb
Adam Young <adamyoungy678@gmail.com>
2023-08-04 23:20:23 +0100

    feat: added global restaurant and items state to basket screen and created reducer function for multiple of the same meal

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,14 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectRestaurant } from '../features/restaurantSlice';
+import { selectBasketItems } from '../features/basketSlice';
 
 const BasketScreen = () => {
   const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
+
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
 
   return (
-    <View>
+    <SafeAreaView>
       <Text>BasketScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -55,7 +55,7 @@ const RestaurantScreen = () => {
         lat,
       })
     );
-  }, [dispatch]);
+  }, []);
 
   useLayoutEffect(() => {
     navigation.setOptions({
