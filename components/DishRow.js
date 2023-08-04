ed59e4e2657bdd3491fe6bd44437064777cd793a
Adam Young <adamyoungy678@gmail.com>
2023-08-04 18:55:02 +0100

    feat: linked redux state to - button

--- components/DishRow.js
+++ components/DishRow.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { urlFor } from "../sanity";
 import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
 import { useDispatch, useSelector } from "react-redux";
-import { addToBasket, selectBasketItems, selectBasketItemsWithId } from "../features/basketSlice";
+import { addToBasket, removeFromBasket, selectBasketItems, selectBasketItemsWithId } from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
@@ -13,6 +13,12 @@ const DishRow = ({ id, name, description, price, image }) => {
 
   const addItemToBasket = () => {
       dispatch(addToBasket({ id, name, description, price, image }))
+  }
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }))
   }
 
   return (
@@ -49,7 +55,7 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity>
+            <TouchableOpacity onPress={removeItemFromBasket}>
               <MinusCircleIcon
                 color="#00CCBB"
                 size={40}
--- features/basketSlice.js
+++ features/basketSlice.js
@@ -12,7 +12,18 @@ export const basketSlice = createSlice({
       state.items = [...state.items, action.payload];
     },
     removeFromBasket: (state, action) => {
-      state.value -= 1;
+      const index = state.items.findIndex((item) => item.id === action.payload.id);
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Can't remove product (id: ${action.payload.id}) as it's not in the basket!`
+        )
+      }
+
+      state.items = newBasket;
     },
   },
 });
