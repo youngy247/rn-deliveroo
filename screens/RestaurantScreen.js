e2a7efedc396b0cdca9daf4fb337c19bb3e31ea9
Adam Young <adamyoungy678@gmail.com>
2023-08-04 15:05:38 +0100

    feat: added name, rating and location to RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,32 +1,43 @@
-import { View, Text, ScrollView, Image, Touchable, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { urlFor } from '../sanity';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import {
+  ArrowLeftIcon,
+  MapPinIcon,
+  StarIcon,
+} from "react-native-heroicons/solid";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
 
-    const {
-      params: {
-        id,
-          imgUrl,
-          title,
-          rating,
-          genre,
-          address,
-          short_description,
-          dishes,
-          long,
-          lat,
-      }
-    } = useRoute();
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat,
+    },
+  } = useRoute();
 
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerShown: false
-      });
-    }, [])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
     <ScrollView>
@@ -37,15 +48,34 @@ const RestaurantScreen = () => {
           }}
           className="h-56 w-full bg-gray-300 p-4"
         />
-        <TouchableOpacity 
-        onPress={() => navigation.goBack()}
-        className='absolute top-14 left-5 p-2 bg-gray-100 rounded-full'
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
         >
           <ArrowLeftIcon size={20} color="#00CCBB" />
         </TouchableOpacity>
       </View>
-    </ScrollView>
-  )
-}
 
-export default RestaurantScreen
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="font-bold text-3xl">{title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500"> {rating}</Text> · {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" size={22} opacity={0.4}  />
+              <Text className="text-xs text-gray-500">Nearby · {address}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default RestaurantScreen;
