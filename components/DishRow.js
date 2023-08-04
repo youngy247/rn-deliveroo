5896693a8ae2dd84db82eaa2eb4851ad9ca56243
Adam Young <adamyoungy678@gmail.com>
2023-08-04 18:57:13 +0100

    feat: conditionally render minus button color to gray when no items in basket and disable touchable opacity

--- components/DishRow.js
+++ components/DishRow.js
@@ -55,9 +55,11 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity onPress={removeItemFromBasket}>
+            <TouchableOpacity 
+              disabled={!items.length}
+              onPress={removeItemFromBasket}>
               <MinusCircleIcon
-                color="#00CCBB"
+                color={items.length > 0 ? "#00CCBB" : "gray"}
                 size={40}
               />
             </TouchableOpacity>
