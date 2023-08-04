f290bd6f56119ae1ff28973baa1bb7861579f488
Adam Young <adamyoungy678@gmail.com>
2023-08-04 23:37:47 +0100

    Implemented basket interface with navigation and delivery details

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,16 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectRestaurant } from '../features/restaurantSlice';
-import { selectBasketItems } from '../features/basketSlice';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { selectBasketItems } from "../features/basketSlice";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -22,10 +29,48 @@ const BasketScreen = () => {
   }, [items]);
 
   return (
-    <SafeAreaView>
-      <Text>BasketScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-gray-400 text-center">
+              {restaurant.title}
+            </Text>
+          </View>
 
-export default BasketScreen
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon height={50} width={50} color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
+
+        <View 
+        className="
+        flex-row 
+        items-center 
+        space-x-4 
+        px-4 py-4
+        bg-white
+        my-5
+        "
+        >
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default BasketScreen;
