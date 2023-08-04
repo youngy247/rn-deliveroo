15c5a31bcd8056c60c4fd5b265b8fab3079de393
Adam Young <adamyoungy678@gmail.com>
2023-08-04 14:48:05 +0100

    feat: removed header on RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const RestaurantScreen = () => {
+  const navigation = useNavigation();
 
     const {
       params: {
@@ -18,6 +19,12 @@ const RestaurantScreen = () => {
           lat,
       }
     } = useRoute();
+
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown: false
+      });
+    }, [])
 
   return (
     <View>
