4d8ee2c0a384f17d5f24c3c9de3def6622273ca7
Adam Young <adamyoungy678@gmail.com>
2023-08-04 14:50:49 +0100

    feat: added image to RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { urlFor } from '../sanity';
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -27,9 +28,16 @@ const RestaurantScreen = () => {
     }, [])
 
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Image
+          source={{
+            uri: urlFor(imgUrl).url(),
+          }}
+          className="h-56 w-full bg-gray-300 p-4"
+        />
+      </View>
+    </ScrollView>
   )
 }
 
