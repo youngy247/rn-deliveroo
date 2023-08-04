d49691287ee191b36241c6f4742f85d39f060ddb
Adam Young <adamyoungy678@gmail.com>
2023-08-04 14:45:27 +0100

    feat: added route params to RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,10 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native';
 
 const RestaurantScreen = () => {
+
+    const {
+      params: {
+        id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat,
+      }
+    } = useRoute();
+
   return (
     <View>
-      <Text>RestaurantScreen</Text>
+      <Text>{title}</Text>
     </View>
   )
 }
