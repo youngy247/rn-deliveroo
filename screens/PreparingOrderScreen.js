994882d90bd6ef9eed9272c2462d5ea2e5bff621
Adam Young <adamyoungy678@gmail.com>
2023-08-05 21:54:20 +0100

    feat: added gif on preparing order screen

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import * as Animatable from 'react-native-animatable'
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-[#00CCBB] justify-center items-center">
+      <Animatable.Image 
+        source={require("../assets/deliveroogif.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
+    </SafeAreaView>
   )
 }
 
