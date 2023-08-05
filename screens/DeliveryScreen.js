c4b6c9ecb1416166fc4301a48317c32f583ebec9
Adam Young <adamyoungy678@gmail.com>
2023-08-05 23:06:09 +0100

    feat: Implement a SafeAreaView to show rider's details including name, image, and a call option

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -22,7 +22,7 @@ const DeliveryScreen = () => {
         </View>
 
         <View className='bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md'>
-          <View className="flex-row justify-between">
+          <View className='flex-row justify-between'>
             <View>
               <Text className='text-lg text-gray-400'>Estimated Arrival</Text>
               <Text className='text-4xl font-bold'>45-55 Minutes</Text>
@@ -35,11 +35,9 @@ const DeliveryScreen = () => {
             />
           </View>
 
-          <Progress.Bar size={30} color="#00CCBB" indeterminate={true} />
+          <Progress.Bar size={30} color='#00CCBB' indeterminate={true} />
 
-          <Text className="mt-3 text-gray-500">
-            Your order at {restaurant.title} is being prepared
-          </Text>
+          <Text className='mt-3 text-gray-500'>Your order at {restaurant.title} is being prepared</Text>
         </View>
       </SafeAreaView>
 
@@ -50,21 +48,35 @@ const DeliveryScreen = () => {
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
-        className="flex-1 mt-10 z-0"
-        mapType="mutedStandard"
+        className='flex-1 mt-10 z-0'
+        mapType='mutedStandard'
       >
-        <Marker 
-        coordinate={{
-          latitude: restaurant.lat,
-          longitude: restaurant.lng,
-        }}
-        title={restaurant.title}
-        description={restaurant.short_description}
-        identifier="origin"
-        pinColor="#00CCBB"
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.lng,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier='origin'
+          pinColor='#00CCBB'
         />
-
       </MapView>
+
+      <SafeAreaView className='bg-white flex-row items-center space-x-5 h-28'>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className='h-12 w-12 bg-gray-300 p-4 rounded-full ml-5'
+        />
+        <View className="flex-1">
+          <Text className='text-lg'>Adam Young</Text>
+          <Text className='text-gray-400'>Your Rider</Text>
+        </View>
+
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold" >Call</Text>
+      </SafeAreaView>
     </View>
   );
 };
