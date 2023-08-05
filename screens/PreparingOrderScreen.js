0507c5191eaf4aaa590b788972579eb8b8497a1f
Adam Young <adamyoungy678@gmail.com>
2023-08-05 21:57:50 +0100

    chore: setup eslint and prettier

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,18 +1,18 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import * as Animatable from 'react-native-animatable'
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import * as Animatable from 'react-native-animatable';
 
 const PreparingOrderScreen = () => {
   return (
-    <SafeAreaView className="flex-1 bg-[#00CCBB] justify-center items-center">
-      <Animatable.Image 
-        source={require("../assets/deliveroogif.gif")}
-        animation="slideInUp"
+    <SafeAreaView className='flex-1 bg-[#00CCBB] justify-center items-center'>
+      <Animatable.Image
+        source={require('../assets/deliveroogif.gif')}
+        animation='slideInUp'
         iterationCount={1}
-        className="h-96 w-96"
+        className='h-96 w-96'
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default PreparingOrderScreen
+export default PreparingOrderScreen;
